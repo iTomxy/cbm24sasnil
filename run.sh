f7540eea56ed317e46e1c74c21bc28bc9e1484d0
iTom <tianyou.liang@student.uts.edu.au>
2024-09-04 16:43:09 +1000

    add tuning cmd for 2nd stage

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -e
 
 echo 1st stage: initial training
 lp=log/1st
-CUDA_VISIBLE_DEVICES=1 python stage1.py totalseg-spineLSpelvic-small --rm_old_ckpt --log_path $lp --data_root ~/data \
+python stage1.py totalseg-spineLSpelvic-small --rm_old_ckpt --log_path $lp --data_root ~/data \
     --batch_size 32 --lr 5e-5 --iter 200 --val_freq 5 --partial spine --window --obviousness 500
 python test.py totalseg-spineLSpelvic-small $lp/ckpt-140.pth
 
@@ -11,4 +11,19 @@ lp=log/2nd
 tw=log/1st/ckpt-140.pth
 python stage2.py totalseg-spineLSpelvic-small --rm_old_ckpt --log_path $lp --data_root ~/data \
     --iter 10000 --val_freq 50 --teacher_weight $tw --ema --ema_start 140 --ema_freq 1 --ema_momentum 0.95
+python test.py totalseg-spineLSpelvic-small $lp/best_val.pth
+
+
+echo 2nd stage: label correction training, use T pred as pseudo at the beginning
+lp=log/2nd-cl140
+tw=log/1st/ckpt-140.pth
+python stage2.py totalseg-spineLSpelvic-small --rm_old_ckpt --log_path $lp --data_root ~/data \
+    --iter 10000 --val_freq 50 --teacher_weight $tw --ema --ema_start 140 --ema_freq 1 --ema_momentum 0.95 --cl_pseudo_start 140
+
+
+echo 2nd stage: label correction training, continue training from 1st ckpt
+lp=log/2nd-re_stu
+tw=log/1st/ckpt-140.pth
+python stage2.py totalseg-spineLSpelvic-small --rm_old_ckpt --log_path $lp --data_root ~/data \
+    --iter 10000 --val_freq 50 --teacher_weight $tw --ema --ema_start 0 --ema_freq 1 --ema_momentum 0.95 --cl_pseudo_start 0 --resume_student
 python test.py totalseg-spineLSpelvic-small $lp/best_val.pth
